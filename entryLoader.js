a1ba7efcc6635a536692247773eb134dff586ce1
watsonhaw5566 <348748267@qq.com>
2025-05-20 11:22:15 +0800

    fix: 更新 loader

--- entryLoader.js
+++ entryLoader.js
@@ -1,9 +1,12 @@
 const VirtualModulesPlugin = require('webpack-virtual-modules');
 const {EntryDependency} = require("@rspack/core");
+
 module.exports = function (source) {
-    new VirtualModulesPlugin({
-        'entry.js': `console.log('entry.js')`
-    })
+    const virtualModules = new VirtualModulesPlugin()
+    if(!virtualModules._compiler){
+        virtualModules.apply(this._compiler)
+        virtualModules.writeModule('entry.js', `console.log('entry.js')`)
+    }
     const dep = new EntryDependency('entry.js')
     this._compilation.addEntry('', dep, {name: 'entry'}, err => {
         if (err) {
